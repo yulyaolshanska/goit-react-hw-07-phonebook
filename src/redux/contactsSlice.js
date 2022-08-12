f229f2a57f17447cee20f2ccb9d5e48c095d6508
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-08-12 19:43:09 +0300

    RTK Query

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,46 +1,41 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
 
-const contactsSlice = createSlice({
-  name: 'contacts',
-  initialState: {
-    items: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  },
-  reducers: {
-    addContact(state, action) {
-      state.items.push(action.payload);
-    },
-    deleteContact(state, action) {
-      state.items = state.items.filter(
-        contact => contact.id !== action.payload
-      );
-    },
-    changeFilter(state, action) {
-      state.filter = action.payload;
-    },
-  },
+export const contactsApi = createApi({
+  reducerPath: 'contacts',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://62f55891ac59075124d029d7.mockapi.io',
+  }),
+  tagTypes: ['Contacts'],
+
+  endpoints: builder => ({
+    getContacts: builder.query({
+      query: () => `/contacts`,
+      providesTags: ['Contacts'],
+    }),
+
+    addContact: builder.mutation({
+      query: values => ({
+        url: `/contacts`,
+        method: 'POST',
+        body: values,
+      }),
+
+      // transformResponse: (response: { data: Post }, meta, arg) => response.data,
+      invalidatesTags: ['Contacts'],
+    }),
+
+    deleteContact: builder.mutation({
+      query: id => ({
+        url: `/contacts/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['Contacts'],
+    }),
+  }),
 });
 
-const persistConfig = {
-  key: 'contacts',
-  storage,
-  blacklist: ['filter'],
-};
-
-export const persistedReducer = persistReducer(
-  persistConfig,
-  contactsSlice.reducer
-);
-
-export const { addContact, deleteContact, changeFilter } =
-  contactsSlice.actions;
-export default contactsSlice;
-export const getContacts = state => state.contacts.items;
-export const getFilter = state => state.contacts.filter;
+export const {
+  useGetContactsQuery,
+  useAddContactMutation,
+  useDeleteContactMutation,
+} = contactsApi;
